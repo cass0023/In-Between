7c39cac9b7177e49d010a839084127b62ad0b3ee
cass cochrane <150483589+cass0023@users.noreply.github.com>
2024-10-23 17:25:37 -0400

    Add enemy scripts

--- DesignWeek-Fall2024/Assets/Scripts/EnemyScripts/SprintGhost.cs
+++ DesignWeek-Fall2024/Assets/Scripts/EnemyScripts/SprintGhost.cs
@@ -2,24 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public class SprintGhost : MonoBehaviour
 {
     public Transform enemySpawnPoint;
     public GameObject enemyPrefab;
     public float enemySpeed = 10;
     private GameObject currentPrefab;
-    void Start(){
+    // Start is called before the first frame update
+    void Start()
+    {
         currentPrefab = null;
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (currentPrefab == null)
+               if (currentPrefab == null)
         {
             var currentEnemy = Instantiate(enemyPrefab, enemySpawnPoint.position, enemySpawnPoint.rotation);
             currentEnemy.GetComponent<Rigidbody>().velocity = transform.forward * enemySpeed;
             currentEnemy.transform.Translate(Vector3.forward * enemySpeed );
             currentPrefab = currentEnemy;
-        }
+        } 
     }
 }
