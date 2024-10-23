27b233ca50d83fb46d850532e95a08caac422f03
cass cochrane <150483589+cass0023@users.noreply.github.com>
2024-10-23 19:45:37 -0400

    Add flashlight timer

--- DesignWeek-Fall2024/Assets/Scripts/LightManager.cs
+++ DesignWeek-Fall2024/Assets/Scripts/LightManager.cs
@@ -6,6 +6,7 @@ public class LightManager : MonoBehaviour
 {
     public KeyCode flashlightInput;
     public GameObject flashlight;
+    public float flashlightTimer = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +16,12 @@ public class LightManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(flashlightInput)){
+        if (Input.GetKey(flashlightInput) && flashlightTimer > 0){
             LightSource();
+            flashlightTimer -= Time.deltaTime;
+            if(flashlightTimer <= 0){
+                flashlightTimer = 0;
+            }
         }
         else{
             flashlight.SetActive(false);
