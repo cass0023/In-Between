8cd0ddf20f9ea42389f2c053a6eb8c4735a0f2fe
cass cochrane <150483589+cass0023@users.noreply.github.com>
2024-10-23 23:18:24 -0400

    fix ceiling ai

--- DesignWeek-Fall2024/Assets/Scripts/EnemyScripts/CeilingDrop.cs
+++ DesignWeek-Fall2024/Assets/Scripts/EnemyScripts/CeilingDrop.cs
@@ -7,14 +7,17 @@ public class CeilingDrop : MonoBehaviour
     public Camera cam;
     public Transform camPos;
     public int delay;
+    private Rigidbody rb;
     void Start(){
         cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        rb = GetComponent<Rigidbody>();
     }
     void Update()
     {
         camPos = cam.transform;
         if (transform.position.z > camPos.position.z + delay){
             transform.position = new Vector3(camPos.position.x, camPos.position.y, camPos.position.z + delay);
+            rb.constraints = RigidbodyConstraints.FreezeAll;
         }
     }
 }
